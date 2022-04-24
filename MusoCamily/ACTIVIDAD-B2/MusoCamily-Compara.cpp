dfe234c40b8a68e26c0baa9a13109a5effa9c560
* <camily.muso.diaz@utelvt.edu.ec>
2022-04-23 23:41:20 -0500

    Actividades B2

---
+++ MusoCamily/ACTIVIDAD-B2/MusoCamily-Compara.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   float V1,V2;
+   cout<<"Ingrese V1: "; cin>>V1;
+   cout<<"Ingrese V2: "; cin>>V2;
+
+if(V1==V2){
+   cout<<"El valor de V1= "<<V1<<" es igual a V2= "<<V2<<endl;
+  
+   }else{
+
+   if(V1<V2){
+   cout<<"el valor de V1= "<<V1<<" ES MENOR A V2= "<<V2<<endl;
+
+   }else{
+
+   cout<<"el valor de V2= "<<V2<<" ES MENOR A V1= "<<V1<<endl;
+ }
+}
+
+return 0;
+
+}
