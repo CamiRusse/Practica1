1eb36e8e91ee3a8ab8b2fa1283f2779c78ce3bd0
javierlu25 <javierlucasangulo@gmail.com>
2022-05-20 18:55:10 -0500

    Contador de monedas-Segunda Version

--- AndreaOrdoñez/ACTIVIDAD-B2-C2/OrdoñezAndrea-CuentaMoneda.cpp
+++ AndreaOrdoñez/ACTIVIDAD-B2-C2/OrdoñezAndrea-CuentaMoneda.cpp
@@ -1,30 +1,47 @@
-#include <iostream>
-using namespace std ;
-int  main()
+// ================================================
+
+// ==> Nombre del programa: Contador de Monedas
+// ==> Archivo : OrdoñezAndrea-CuentaMoneda.cpp
+// ==>Autor: Andrea Ordoñez
+// ==>Fecha de elaboración: 2022-05-02
+// ==>Fecha última actualización: 2022-05-20
+// ================================================ =
+#include<iostream>
+using namespace std;
+int  main ()
 {
-	int n, k= 0 , k1= 0 , k2= 0 ;
-	float u, m= 0 , m1= 0 , m2= 0 ;
-	cout<< "Escriba la cantidad de monedas: " ;
-	cin>>n;
-	do {
-		cout<< " Ingrese el valor de las monedas (0.10,0.25): " ;
-		cin>>u;
-		k=k+1;
-		m=m+u;
-		if (u==0.10){
-			k1=k1+1;
-			m1=m1+n;
+	int AB_n, AB_k= 0 , AB_k1= 0 , AB_k2= 0 ;
+	float AB_x, AB_i= 0 , AB_i1= 0 , AB_i2= 0 ;
+	cout<< "Escriba el valor de monedas a contar :" <<endl;
+	cin>>AB_n;
+	do{
+		cout<< " Valor de monedas (0.10,0.25): " ;
+		cin>>AB_x;
+		AB_k=AB_k+ 1 ;
+		AB_i=AB_i+AB_x;
+		if (AB_x== 10 ){
+			AB_k1=AB_k1+ 1 ;
+			AB_i1=AB_i1+AB_x;
 		} else {
-		k2=k2+1;
-		m2=m2+n;
+		AB_k2=AB_k2+ 1 ;
+		AB_i2=AB_i2+AB_x;
 		}
-	} while (k<n);
-	cout<< " Mostre resultado: " <<endl;
-	cout<< "La cantidad de monedas añadidas es: " <<k<<endl;
-	cout<< "El resultado total del dinero contado es: " <<m<<endl;
-	cout<< " La cantidad de monedas de 0.10c añadidas es: " <<k1<<endl;
-	cout<< " Resultado total de dinero en monedas de 0.10cc: " <<m1<<endl;
-	cout<< "Cantidad de monedas de 0,25cc añadidas: " <<k2<<endl;
-	cout<< "La cantidad del dinero en monedas de 0.25cc es: " <<m2<<endl;
-	return 0;
+		
+		cout<<endl<< " //========================================= ======= " <<endl;
+cout<< " //==> Nombre del programa: Contador de Monedas " <<endl;
+cout<< " //==> Archivo : OrdoñezAndrea-CuentaMoneda.cpp " <<endl;
+cout<< " //==>Autor: Andrea Ordoñez " <<endl;
+cout<< " //==>Fecha de elaboración: 2022-05-02 " <<endl;
+cout<< " //==>Fecha ultima actualización: 2022-05-20 " <<endl;
+cout<< " //============================================ ===== " <<endl;
+
+} while (AB_k<AB_n);
+	cout<< " Obtenemos de resultado: " <<endl;
+	cout<< " El valor de monedas ingresadas: " <<AB_k<<endl;
+	cout<< " El valor total del dinero sumado: " <<AB_i<<endl;
+	cout<< " El valor de monedas de 0.10 ingresadas: " <<AB_k1<<endl;
+	cout<< " El valor total de monedas de 0.10: " <<AB_i1<<endl;
+	cout<< " El valor de monedas de 0.25 ingresadas: " <<AB_k2<<endl;
+	cout<< " El valor total de monedas de 0.25: " <<AB_i2<<endl;
+	return 0 ;
 }
